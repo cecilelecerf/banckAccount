8da3e36b76f89617790591209fde0d44f45261c0
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-12 16:16:23 +0200

    use all method

--- src/index.php
+++ src/index.php
@@ -2,5 +2,12 @@
 require_once("./class/bankAccount.php");
 $bankAccount = new BankAccount(3000, "Lecerf", "0783058963", "cece.lecerf@gmail.com");
 var_dump($bankAccount);
+echo "<br>";
+echo $bankAccount->toString();
+echo "<br>";
+$bankAccount->withdrawal(200);
+echo $bankAccount->toString();
+echo "<br>";
+$bankAccount->deposit(200);
 echo $bankAccount->toString();
 ?>
