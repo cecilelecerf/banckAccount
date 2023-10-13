3ced9fc744525f4255210ce733180731c3506d8a
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-13 16:53:03 +0200

    dockerized

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php 
 require_once("./class/bankAccount.php");
 $bankAccount = new BankAccount(3000, "Lecerf", "0783058963", "cece.lecerf@gmail.com");
-var_dump($bankAccount);
 echo "<br>";
 echo $bankAccount->toString();
 echo "<br>";
